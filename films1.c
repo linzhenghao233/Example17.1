f4044c677e82e2279924d0869d1995a63bda38bc
linzhenghao233 <1392872316@qq.com>
2023-09-15 23:24:52 +0800

    0915

--- films1.c
+++ films1.c
@@ -23,4 +23,14 @@ int main(void) {
 			continue;
 		puts("Enter next movie title (empty line to stop):");
 	}
+	if (i == 0)
+		printf("No data entered. ");
+	else
+		printf("Here is the the movie list:\n");
+
+	for (j = 0; j < i; j++)
+		printf("Movie: %s Rating: %d\n", movies[j].title, movies[j].rating);
+	printf("Bye!\n");
+
+	return 0;
 }
